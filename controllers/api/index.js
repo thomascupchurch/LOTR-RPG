1fa06efd8eac5e2bb82a24f65c8ba1f09f3eac6b
mjh1985codeman <Michael.Hodges1985@Yahoo.com>
2021-09-13 18:59:19 -0500

    added user Model and routes

---
+++ controllers/api/index.js
@@ -0,0 +1,7 @@
+const router = require("express").Router();
+
+const userRoutes = require("./user-routes.js"); //done.
+
+router.use("/users", userRoutes); //done.
+
+module.exports = router;
---
+++ models/index.js
@@ -0,0 +1,9 @@
+//import all models.
+const User = require("./User");
+
+//create associations.
+
+//User associations.
+
+// export models.
+module.exports = { User };
