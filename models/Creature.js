493bce197cad4371a417c0c7edfece8d3b033316
Boba Fett <thomascupchurch@gmail.com>
2021-09-13 15:54:01 -0500

    individual models

---
+++ models/Creature.js
@@ -0,0 +1,8 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Creature extends Model {}
+
+Creature.init({
+    
+})
---
+++ models/Dwarf.js
@@ -0,0 +1,8 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Dwarf extends Model {}
+
+Dwarf.init({
+    
+})
