08182648967717741525e07ab09d7c48b1473902
Boba Fett <thomascupchurch@gmail.com>
2021-09-16 09:59:23 -0500

    import models into game.js

--- models/index.js
+++ models/index.js
@@ -1,9 +1,11 @@
 //import all models.
 const User = require("./User");
+const Scores = require("./Scores");
+const Character = require("./Character");
 
 //create associations.
 
 //User associations.
 
 // export models.
-module.exports = { User };
+module.exports = { User, Character, Scores };
