8b1ea05d113a38c865a27a0e0ef504e6d3013ec6
mjh1985codeman <Michael.Hodges1985@Yahoo.com>
2021-09-15 21:40:22 -0500

    login functionality complete

---
+++ public/javascript/login.js
@@ -0,0 +1,58 @@
+async function loginFormHandler(event) {
+  event.preventDefault();
+
+  const user = document.querySelector("#user-login").value.trim();
+  const password = document.querySelector("#password-login").value.trim();
+
+  if (user && password) {
+    const response = await fetch("/api/users/login", {
+      method: "post",
+      body: JSON.stringify({
+        user,
+        password,
+      }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+      document.location.replace("/character/");
+    } else {
+      alert("There was an issue with your login, please try again.");
+      //Add to add the "await" to capture the response properly.
+      const data = await response.json();
+      console.log(data);
+    }
+  }
+}
+
+async function signupFormHandler(event) {
+  event.preventDefault();
+
+  const username = document.querySelector("#username-signup").value.trim();
+  const password = document.querySelector("#password-signup").value.trim();
+
+  if (username && password) {
+    const response = await fetch("/api/users", {
+      method: "post",
+      body: JSON.stringify({
+        username,
+        password,
+      }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+      document.location.replace("/character/");
+    } else {
+      alert("oops looks like there was a problem, please try again.");
+    }
+  }
+}
+
+document
+  .querySelector(".login-form")
+  .addEventListener("submit", loginFormHandler);
+
+document
+  .querySelector(".signup-form")
+  .addEventListener("submit", signupFormHandler);
