bbe4f247b8bdc3b57c8da2677526af8e3adf3b1b
mjh1985codeman <Michael.Hodges1985@Yahoo.com>
2021-09-16 21:27:00 -0500

    created create user page and confirmed new character is writing to the characters table

--- controllers/api/character-routes.js
+++ controllers/api/character-routes.js
@@ -1,1 +1,33 @@
+const router = require("express").Router();
+const withAuth = require("../../utils/auth");
 
+const { Character } = require("../../models");
+// const { route } = require("./user-routes");
+
+//get all characters
+//example: http://localhost:3001/api/characters
+router.get("/", withAuth, (req, res) => {
+  Character.findAll()
+    .then((dbCharacterData) => res.json(dbCharacterData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+//create character route.
+router.post("/", withAuth, (req, res) => {
+  // expects {char_name: "Frodo", char_type: "Hobbit", user_id: 1 }
+  Character.create({
+    char_name: req.body.char_name,
+    char_type: req.body.char_type,
+    user_id: req.session.user_id,
+  })
+    .then((dbCharacterData) => res.json(dbCharacterData))
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json(err);
+    });
+});
+
+module.exports = router;
---
+++ public/javascript/create-character.js
@@ -0,0 +1,28 @@
+async function newFormHandler(event) {
+  event.preventDefault();
+
+  const char_name = document.querySelector('input[name="charName"]').value;
+  const char_type = document.querySelector('select[name="char-type"]').value;
+
+  const response = await fetch("/api/characters", {
+    method: "POST",
+    body: JSON.stringify({
+      char_name,
+      char_type,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  if (response.ok) {
+    // document.location.replace("/game");
+    window.alert("Character created!");
+  } else {
+    alert(response.statusText);
+  }
+}
+
+document
+  .querySelector(".create-char-form")
+  .addEventListener("submit", newFormHandler);
