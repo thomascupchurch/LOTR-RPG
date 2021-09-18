f11b9d90ec8fd823539775b4eebe04aefaa02645
mjh1985codeman <Michael.Hodges1985@Yahoo.com>
2021-09-17 23:11:31 -0500

    began work on game routes

--- controllers/index.js
+++ controllers/index.js
@@ -3,10 +3,12 @@ const router = require("express").Router();
 const homeRoutes = require("./homepage-routes");
 const characterPageRoutes = require("./characterpage-routes");
 const scoresPageRoutes = require("./scorepage-routes");
+const gamePageRoutes = require("./gamepage-routes");
 const apiRoutes = require("./api/");
 
 router.use("/", homeRoutes);
 router.use("/", characterPageRoutes);
+router.use("/", gamePageRoutes);
 router.use("/", scoresPageRoutes);
 router.use("/api", apiRoutes);
 
