d2be2cf83d3655152cb268cca9c036a52679020b
mjh1985codeman <Michael.Hodges1985@Yahoo.com>
2021-09-19 23:23:11 -0500

    added music for the create char and game page.

---
+++ public/javascript/char-music.js
@@ -0,0 +1,6 @@
+let charMusic = new Howl({
+  src: ["/music/250856__joshuaempyre__epic-orchestra-loop.wav"],
+  volume: 0.2,
+});
+
+charMusic.play();
---
+++ public/javascript/game-music.js
@@ -0,0 +1,7 @@
+let gameMusic = new Howl({
+  src: ["/music/250972__luis-audp__celtic-tin-whistle-pennywhistle-in-d.mp3"],
+  volume: 0.3,
+  loop: true,
+});
+
+gameMusic.play();
