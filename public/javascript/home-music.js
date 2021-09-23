3e4aee6fef182d9f6f11e128b3522df4f0dc4e78
mjh1985codeman <Michael.Hodges1985@Yahoo.com>
2021-09-23 10:28:27 -0500

    added audio control to login page

--- public/javascript/home-music.js
+++ public/javascript/home-music.js
@@ -3,4 +3,17 @@ let loginMusic = new Howl({
   volume: 0.2,
 });
 
+let elms = ["playBtn", "pauseBtn"];
+elms.forEach(function (elm) {
+  window[elm] = document.getElementById(elm);
+});
+
+// Bind our player controls.
+playBtn.addEventListener("click", function () {
+  loginMusic.play();
+});
+pauseBtn.addEventListener("click", function () {
+  loginMusic.pause();
+});
+
 loginMusic.play();
